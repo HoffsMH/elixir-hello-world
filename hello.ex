a29e9a0bb822668565d22a8a41c24aa649479b4a
HoffsMH <hecker.matthew@gmail.com>
2018-01-23 14:11:20 -0500

    more!

--- hello.ex
+++ hello.ex
@@ -9,6 +9,24 @@ x = 0.1 + 0.2
 IO.puts("whaat");
 IO.puts("whaat");
 IO.puts("whaat");
+IO.puts('whaat');
+# why does the doc indicate that only double quotes will work
+IO.puts('dziękuję')
+
+# this will go ahead and print the whole word out to the screen
+IO.puts('hełło')
+
+
+# I wonder what this will do then
+IO.inspect('hełło')
+# looks like we get back the array
+
+
+IO.inspect('whaat');
+# this outputs 'whaat' which kind of seems inconsistent to me
+
+IO.puts(List.first('hello')); # this will work
+# IO.puts(List.first("hello")); # this will not
 
 # Ha looks like not even the mighty elixir can escape this issue
 IO.puts(x);
