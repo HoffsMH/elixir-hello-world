56722dda9b2f6582d921eaa13a32b33e2580f2fe
HoffsMH <hecker.matthew@gmail.com>
2018-01-23 13:57:47 -0500

    more playing around

--- hello.ex
+++ hello.ex
@@ -1,3 +1,11 @@
 :crypto.strong_rand_bytes 3
 
+
+x = 0.1 + 0.2
+
 IO.puts("whaat");
+IO.puts("whaat");
+IO.puts("whaat");
+
+# Ha looks like not even the mighty elixir can escape this issue
+IO.puts(x);
