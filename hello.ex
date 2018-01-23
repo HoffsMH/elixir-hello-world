885a5a809ece4624e07c3cc2b536b005f592a3ef
HoffsMH <hecker.matthew@gmail.com>
2018-01-23 14:04:55 -0500

    comments

--- hello.ex
+++ hello.ex
@@ -1,3 +1,6 @@
+
+#  The point of this is that we are accessing something in native
+# erlang?
 :crypto.strong_rand_bytes 3
 
 
@@ -9,3 +12,4 @@ IO.puts("whaat");
 
 # Ha looks like not even the mighty elixir can escape this issue
 IO.puts(x);
+IO.inspect(MyModule.MyApp)
